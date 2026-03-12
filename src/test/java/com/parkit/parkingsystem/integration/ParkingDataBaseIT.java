fc655bd0c4bbefd84f5bf73a8d18597939f5a363
Ilian Metina <ilian.metina83@gmail.com>
2026-03-12 16:29:34 +0100

    add

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -5,6 +5,7 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -55,9 +56,10 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         // Appeler ticket.getTicket() et vérifier qu'il soit bien en BDD
-        ticketDAO.getTicket("ABCDEF");
-        assertNotNull(ticketDAO);
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket);
         int nextSpot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+        assertNotEquals(1, nextSpot);
         // Vérifier que nextSpot != 1 car si nextSpot = 1, la voiture n'est pas correctement enregistrée
         // car sinon available serait égal à 0
         //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
@@ -69,7 +71,9 @@ public class ParkingDataBaseIT {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket.getOutTime());
+        assertEquals(0, ticket.getPrice());
         //TODO: check that the fare generated and out time are populated correctly in the database
     }
 
