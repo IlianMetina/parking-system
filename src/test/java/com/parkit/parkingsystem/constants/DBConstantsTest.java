3690c1e18086f6d4a2935c9678aedb08cadda84d
Ilian Metina <ilian.metina83@gmail.com>
2026-03-13 14:54:59 +0100

    adds

---
+++ src/test/java/com/parkit/parkingsystem/constants/DBConstantsTest.java
@@ -0,0 +1,22 @@
+package com.parkit.parkingsystem.constants;
+
+import org.junit.jupiter.api.Test;
+
+
+class DBConstantsTest {
+
+    @Test
+    public void dbConstantsTest(){
+
+        String firstQuery = DBConstants.GET_NEXT_PARKING_SPOT;
+        String secondQuery = DBConstants.GET_TICKET;
+        String thirdQuery = DBConstants.UPDATE_PARKING_SPOT;
+        String fourthQuery = DBConstants.IS_CLIENT_LOYAL;
+        String fifthQuery = DBConstants.UPDATE_TICKET;
+        String sixthQuery = DBConstants.SAVE_TICKET;
+
+
+
+    }
+
+}
